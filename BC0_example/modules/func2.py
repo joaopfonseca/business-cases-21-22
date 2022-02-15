af196d1e7961fd19f834d1dd3e4946d2908b53d7
hugosaisse <55488468+hugosaisse@users.noreply.github.com>
2022-02-15 02:50:57 +0000

    Updated BC0

---
+++ BC0_example/modules/func2.py
@@ -0,0 +1,5 @@
+# defining a function hello()
+def hello():
+    
+    # printing the string 'hello!'
+    print('hello!')
---
+++ BC0_example/modules/modules4/func4.py
@@ -0,0 +1,4 @@
+import inspect
+
+def my_location():
+    return inspect.getsourcefile(my_location)
